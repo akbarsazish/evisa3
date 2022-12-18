50c1ea742eae00d118386de888dac3cf2fc563db
akbarsazish <sazishaliakbar@gmail.com>
2022-12-18 22:56:40 +0330

    from sazish side

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -57,7 +57,7 @@
                     <a class="nav-link" href="{{url('adminList')}}" > <i class="fa fa-list fa-lg" aria-hidden="true"></i>  لیست کاربر   </a>
                 </li>
                 <li class="nav-item">
-                    <img style="width:35px; height:35px" src="{{url('resources/assets/images/logo.png')}}" alt="logo" class="responsive">
+                    <img style="width:50px; height:40px" src="{{url('resources/assets/images/logo.png')}}" alt="logo" class="responsive">
                 </li>
         </ul>
 </div>
